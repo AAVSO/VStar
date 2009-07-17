e8a0d8fae0068844b76978f681914808b09f5553
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-07-17 12:33:14 +0000

    Descriptive stats UTs

--- trunk/test/org/aavso/tools/vstar/util/DescStatsTest.java
+++ trunk/test/org/aavso/tools/vstar/util/DescStatsTest.java
@@ -29,19 +29,27 @@ import org.aavso.tools.vstar.data.ValidObservation;
 import org.aavso.tools.vstar.data.validation.InclusiveRangePredicate;
 
 /**
- * Unit tests for descriptive stats class.
+ * Unit tests for descriptive stats class. Sample data taken from chapter 10 of
+ * the AAVSO's Hands-on Astrophysics.
  */
 public class DescStatsTest extends TestCase {
 
-	private static double[] mags1 = {1,2,3,4,5};
-	private static double[] mags2 = {2,3,3,3,4};
-	
+	private static InclusiveRangePredicate jdRange = new InclusiveRangePredicate(
+			2450000, 2450002);
+
+	private static double[] mags1 = { 1, 2, 3, 4, 5 };
+	private static double[] mags2 = { 2, 3, 3, 3, 4 };
+	private static double[] mags3 = { 4.0, 3.9, 4.1, 4.0, 4.2, 3.9, 3.9, 4.1,
+			3.8, 4.0 };
+
 	private List<ValidObservation> observations1;
 	private List<ValidObservation> observations2;
-	
+	private List<ValidObservation> observations3;
+
 	private InclusiveRangePredicate jdRange1;
 	private InclusiveRangePredicate jdRange2;
-	
+	private InclusiveRangePredicate jdRange3;
+
 	/**
 	 * Constructor
 	 * 
@@ -49,62 +57,84 @@ public class DescStatsTest extends TestCase {
 	 */
 	public DescStatsTest(String name) {
 		super(name);
-	
-		int jdIncrement1 = 0;		
-		this.observations1 = new ArrayList<ValidObservation>();
-		for (double mag : mags1) {
-			ValidObservation obs = new ValidObservation();
-			obs.setMagnitude(new Magnitude(mag, MagnitudeModifier.NO_DELTA, false));
-			obs.setDateInfo(new DateInfo(2450000));
-			this.observations1.add(obs);
-			jdIncrement1 += 0.5;
-		}
-		
-		this.jdRange1 = new InclusiveRangePredicate(2450000, 2450002);
-		
-		int jdIncrement2 = 0;		
-		this.observations2 = new ArrayList<ValidObservation>();
-		for (double mag : mags2) {
-			ValidObservation obs = new ValidObservation();
-			obs.setMagnitude(new Magnitude(mag, MagnitudeModifier.NO_DELTA, false));
-			obs.setDateInfo(new DateInfo(2450000));
-			this.observations2.add(obs);
-			jdIncrement2 += 0.5;
-		}
 
-		this.jdRange2 = new InclusiveRangePredicate(2450000, 2450002);
+		this.observations1 = populateObservations(mags1);
+		this.jdRange1 = jdRange;
+
+		this.observations2 = populateObservations(mags2);
+		this.jdRange2 = jdRange;
+
+		this.observations3 = populateObservations(mags3);
+		this.jdRange3 = jdRange;
 	}
 
 	// Valid
-	
+
 	public void testMeanSample1() {
-		double magMean = DescStats.calcMagMeanInJDRange(this.observations1, this.jdRange1);
-		
+		double magMean = DescStats.calcMagMeanInJDRange(this.observations1,
+				this.jdRange1);
+
 		assertEquals(3.0, magMean);
 	}
 
 	public void testMeanSample2() {
-		double magMean = DescStats.calcMagMeanInJDRange(this.observations2, this.jdRange2);
-		
+		double magMean = DescStats.calcMagMeanInJDRange(this.observations2,
+				this.jdRange2);
+
 		assertEquals(3.0, magMean);
 	}
 
+	public void testMeanSample3() {
+		double magMean = DescStats.calcMagMeanInJDRange(this.observations3,
+				this.jdRange3);
+		String magMeanStr = String.format("%1.1f", magMean);
+		assertEquals("4.0", magMeanStr);
+	}
+
 	public void testStdDevSample1() {
-		double magStdDev = DescStats.calcMagSampleStdDevInJDRange(this.observations1, this.jdRange1);
+		double magStdDev = DescStats.calcMagSampleStdDevInJDRange(
+				this.observations1, this.jdRange1);
 		String magStdDevStr = String.format("%1.1f", magStdDev);
 		assertEquals("1.6", magStdDevStr);
 	}
 
 	public void testStdDevSample2() {
-		double magStdDev = DescStats.calcMagSampleStdDevInJDRange(this.observations2, this.jdRange2);		
+		double magStdDev = DescStats.calcMagSampleStdDevInJDRange(
+				this.observations2, this.jdRange2);
 		String magStdDevStr = String.format("%1.2f", magStdDev);
 		assertEquals("0.71", magStdDevStr);
 	}
-	
-//	public void testStdErrorOfAverageSample3() {
-//		double magStdDev = DescStats.calcMagSampleStdDevInJDRange(this.observations3, this.jdRange3);
-//		String magStdDevStr = String.format("%1.1f", magStdDev);
-//		assertEquals("1.6", magStdDevStr);
-//	}
-	
+
+	public void testStdDevSample3() {
+		double magStdDev = DescStats.calcMagSampleStdDevInJDRange(
+				this.observations3, this.jdRange3);
+		String magStdDevStr = String.format("%1.2f", magStdDev);
+		assertEquals("0.12", magStdDevStr);
+	}
+
+	public void testStdErrorOfAverageSample3() {
+		double magStdDev = DescStats.calcMagStdErrorOfAverageInJDRange(
+				this.observations3, this.jdRange3);
+		String magStdDevStr = String.format("%1.3f", magStdDev);
+		assertEquals("0.038", magStdDevStr);
+	}
+
+	// Helpers
+
+	// Populates and returns a list of valid observations with supplied
+	// magnitude values and bogus JD values.
+	private List<ValidObservation> populateObservations(double[] mags) {
+		int jdIncrement = 0;
+		List<ValidObservation> observations = new ArrayList<ValidObservation>();
+		for (double mag : mags) {
+			ValidObservation obs = new ValidObservation();
+			obs.setMagnitude(new Magnitude(mag, MagnitudeModifier.NO_DELTA,
+					false));
+			obs.setDateInfo(new DateInfo(2450000));
+			observations.add(obs);
+			jdIncrement += 0.5;
+		}
+
+		return observations;
+	}
 }
