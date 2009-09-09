a781f242d45323e97bed466df740176888a25397
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-09-09 05:55:50 +0000

    Fixed ant dist target to not include jars only VStar classes and resources; updated readme text; added work-in-progress comment to JNLP; reordered Mac L&F code in main().

--- trunk/src/org/aavso/tools/vstar/ui/dialog/AboutBox.java
+++ trunk/src/org/aavso/tools/vstar/ui/dialog/AboutBox.java
@@ -61,7 +61,7 @@ public class AboutBox {
 				.append("This project was funded in part by grant No. 000379097 from the\n");
 		strBuf.append("National Science Foundation.\n\n");
 
-		strBuf.append("Code by: David Benn\n");
+		strBuf.append("Lead Developer: David Benn\n");
 		strBuf.append("Contact: aavso@aavso.org\n");
 		strBuf.append("License: GNU Affero General Public License\n\n");
 		strBuf.append("Images were sourced from:\n\n");
