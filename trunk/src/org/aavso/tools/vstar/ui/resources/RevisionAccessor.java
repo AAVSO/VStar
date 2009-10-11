7264aed93c7b763ae9424b77cb4fbf5fe94a299a
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-10-11 07:37:29 +0000

    Manifest file now removed by Ant after jar created. Removed vstar.bat and vstar.sh. VStar should now either be invoked via Ant or jar double-click.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "29:225MP";
+	private static String REVISION = "29:227MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
--- trunk/vstar.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-java -cp extlib\jcommon-1.0.16.jar;extlib\mysql-connector-java-5.1.8-bin.jar;extlib\jfreechart-1.0.13.jar;extlib\swing-worker-1.2.jar;extlib\jlfgr-1_0.jar;dist\vstar.jar org.aavso.tools.vstar.ui.VStar
