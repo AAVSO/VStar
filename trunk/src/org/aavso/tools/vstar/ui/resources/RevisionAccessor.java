ff6f44be125d0d24b178d3ec10f3d364ac215de2
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-10-26 13:08:34 +0000

    Moved to use of DriverManager and added call to setLoginTimeout(); not convinced it's doing anything however.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "29:241MP";
+	private static String REVISION = "29:242MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
