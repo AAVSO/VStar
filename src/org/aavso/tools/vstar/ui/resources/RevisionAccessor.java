f11cce583a8fcaaa035cd20f5a61f71f910baa8c
mu301 <mu301@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-12-13 15:24:44 +0000

    Minor change to phase plot tooltip.  Update ChangeLog.  Commit vstar.jar in preperation for new CS deployment.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "294:338MP";
+	private static String REVISION = "339M";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
