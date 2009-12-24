2382a711b3d0742a67beae8c740d263ad02d0488
mu301 <mu301@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-12-24 15:11:36 +0000

    Increment revision number.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "358M";
+	private static String REVISION = "358:361";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
