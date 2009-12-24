46b0b6ff3c9a2ddfd6251fa3b3164170616d537c
mu301 <mu301@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-12-24 15:05:05 +0000

    Correct ChangeLog and update revision number.  Commit vstar.jar for CS deploy.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "355:356";
+	private static String REVISION = "358M";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
