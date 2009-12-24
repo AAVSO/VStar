48ffaf167e731ce09b4757097b9fc1a9a42d6e25
mu301 <mu301@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2009-12-24 14:13:03 +0000

    Correct typo in ChangeLog.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "355M";
+	private static String REVISION = "355:356";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
