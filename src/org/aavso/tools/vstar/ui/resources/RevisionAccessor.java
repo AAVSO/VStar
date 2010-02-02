4fbff7502b0e7227b4479b20e378a1b1c86fb597
david_benn <4115628+dbenn@users.noreply.github.com>
2010-02-02 14:14:58 +0000

    Added comments to ValidationType re: different chars for pre-validated in database (Z) and download format (P). Changed regex in CommentType to permit anything in comment code string so we don't exclude otherwise good data. Anything not known by CommentType will be treated as "other". See tracker 2915572. Also added a small comment change to DescStats.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:408MP";
+	private static String REVISION = "370:409MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
