11031b34810792f639806fdce5a0834e4a1c9c65
david_benn <4115628+dbenn@users.noreply.github.com>
2010-03-23 14:30:44 +0000

    Code clean-up for tracker 2942299 (period analysis: DCDFT).

--- src/org/aavso/tools/vstar/util/period/DateCompensatedDiscreteFourierTransform.java
+++ src/org/aavso/tools/vstar/util/period/DateCompensatedDiscreteFourierTransform.java
@@ -57,13 +57,13 @@ import org.aavso.tools.vstar.data.ValidObservation;
 // - If possible, avoid having to copy any data at all, i.e. skip load_raw().
 // - Also be able to retrieve via getters info in header of generated
 // .ts file, e.g.
-// DCDFT File=delcep.vis NUM= 3079 AVE= 3.9213 SDV=
-// 0.2235 VAR= 0.0500
-// JD 2450000.2569-2450999.7097 T.AVE=2450446.0000
+//   DCDFT File=delcep.vis NUM= 3079 AVE= 3.9213 SDV=
+//   0.2235 VAR= 0.0500
+//   JD 2450000.2569-2450999.7097 T.AVE=2450446.0000
 // - Double check with Matt that we are doing the date compensation
 // part in dcdft() here.
 // - Is DCDFT primarily intended to solve the aliasing problem? See
-// cataclysmic variable book.
+// cataclysmic variable book ref.
 
 public class DateCompensatedDiscreteFourierTransform {
 
@@ -72,13 +72,11 @@ public class DateCompensatedDiscreteFourierTransform {
 
 	// private Map<String, List<DcDftDataPoint>> series;
 
-	// private double dang0;
 	private double dangcut;
 	private double damp;
 	private double damp2;
 	private double dave;
 	private double dcoef[] = new double[51];
-	// private double dd;
 	private double dfouramp2;
 	private double dfpow;
 	private double dfre[] = new double[21];
@@ -91,95 +89,46 @@ public class DateCompensatedDiscreteFourierTransform {
 	private double dlper;
 	private double dlpower;
 	private double dmat[][] = new double[51][51];
-	// private double dphase;
-	// private double dpow;
 	private double dpower;
 	private double dsig;
-	// private double dsol;
-	// private double dt;
 	private double dt0;
 	private double dtsig;
 	private double dtave;
-	// private double dts;
 	private double dtscale;
 	private double dtvar;
 	private double dtzero;
 	private double dvar;
 	private double dvec[] = new double[51];
-	// private double dw;
 	private double dweight;
-	// private double dx;
-	// private double dxout;
 	private double ff;
 	private double hifre;
-	// private int ii;
-	// private int jj;
 	private int ma;
-	// private int magmark;
 	private int magspan;
-	// private int mazoom;
 	private int mb;
-	// private int mbzoom;
-	// private int mcur;
-	// private int mflag;
 	private int mhigh;
 	private int mlow;
-	// private int mput;
-	// private int na;
-	// private int nactual;
-	// private int nb;
 	private int nbias;
-	// private int nbins;
-	// private int nbottom;
 	private int nbrake;
 	private int ndigt;
 	private int ndim;
 	private int ndim2;
-	// private int nfit[] = new int[1000001];
 	private int nfre;
-	// private int ni;
 	private int nj;
-	// private int nk;
 	private int nlolim;
 	private int npoly;
-	// private int nright;
-	// private int ntcol;
-	// private int nthis;
-	// private int ntop;
 	private int numact;
 	private int numraw;
-	// private int numred;
 	private int nuplim;
-	// private int nx;
-	// private int nzoom;
 	private String obias[] = new String[51];
 	private String obs[];
-	// private double pp;
-	// private double sfit[] = new double[1000001];
-	// private double tcur;
-	// private double tfit[] = new double[1000001];
 	private double tlolim;
-	// private double tlozoom;
 	private double tmark;
-	// private double toff;
-	// private double toffl;
-	// private double tput;
 	private double tresolv;
-	// private double tsize;
 	private double tuplim;
 	private double tuplimit;
-	// private double tupzoom;
 	private double tvec[];
-	// private double twopi;
 	private double wvec[];
-	// private double xfit[] = new double[1000001];
-	// private double xleft;
-	// private double xright;
 	private double xvec[];
-
-	// private double xx;
-	// private double ybottom;
-	// private double ytop;
 
 	// -------------------------------------------------------------------------------
 
@@ -243,64 +192,29 @@ public class DateCompensatedDiscreteFourierTransform {
 		load_raw();
 		dcdft();
 		statcomp();
-		// TODO: now, where are the results...? see line 432
 	}
 
 	// -------------------------------------------------------------------------------
 
 	protected void load_raw() {
 
-		// String fin, flog;
 		double dtspan, x, dd, dtcorr, dx;
 		double jda, jdb;
-		// int ijda, ijdb;
-		// int idot, ispace;
 		int num, n;
 		double deetee, deex;
-
-		// ijda = 0;
-		// ijdb = 0;
-
-		// idot=index[fin]['.'];
-		// ispace=index[fin][' '];
-		// if (idot==0) idot=ispace;
-		// flog=fin[1:idot-1]//'.ts';
-		// open[unit=9,file=fin][status='old'];
-		// open[unit=1,file=flog][status='unknown'];
 
 		ma = 999999;
 		mb = -999999;
 		num = 0;
-		// write(6,*) 'start time = (JD 0000000)'
-		// read*,ijda
-		// read[5][909] ijda;
-		// }
-		// read*,ijdb
-		// read[5][909] ijdb;
-		// if (ijdb == 0)
-		// ijdb = 2500000;
-		// 909 format(i7)
-		// TODO: set these to min/max JD from obs list
-		// jda = (double) ijda;
-		// jdb = (double) ijdb;
 		jda = observations.get(0).getJD();
 		jdb = observations.get(observations.size() - 1).getJD();
 
-		// TODO: change this to iterate over list
-		// for (nxx = 1; nxx <= 1000000; nxx++) {
 		for (ValidObservation observation : this.observations) {
-			// read(9,*,end=99) deetee,deex
-			// if (deetee<jda) goto 97;
-			// if (deetee>jdb) goto 99;
 			num = num + 1;
-			// TODO: get detee and deex from obs list!
-			// deetee = 0; // JD
-			// deex = 0; // mag
 			deetee = observation.getJD();
 			deex = observation.getMag();
 			tvec[num] = deetee;
 			xvec[num] = deex;
-			// System.out.println(tvec[num] + " " + xvec[num]);
 			if (num == 1)
 				dt0 = (int) (tvec[num]); // TODO: do this once at start?
 
@@ -330,8 +244,6 @@ public class DateCompensatedDiscreteFourierTransform {
 			if (xvec[num] > (double) mb)
 				mb = (int) (xvec[num]);
 		}
-		// close[9];
-		// num=num-1
 		ma = ma - 1;
 		mb = mb + 2;
 		mlow = ma;
@@ -383,24 +295,6 @@ public class DateCompensatedDiscreteFourierTransform {
 		// or
 		// global?
 		int nbest;
-		// int nj, iff, ixx, nbest, ipp;
-		// double ff, res, hiper, xloper, pper;
-		// String rfil;
-		// String rname;
-		// double avemod, varmod;
-		// int nb, np, nn, na;
-		// double dd;
-		// double ttl, xml, resid, residl, tt, xm, rdev;
-		// int n;
-		// double[] dtest = new double[21];
-		// double[] dres = new double[21];
-		// int nvariable, nvary, nlocked, nsofar, nv, nvlast, nchange;
-		// double dbpower;
-		// int iwhile1, iwhile2, iswap;
-		// int i, j;
-
-		// iname=index[fin][' '];
-		// fprint=fin(1:iname)
 
 		npoly = 0;
 		nbest = 20;
@@ -427,30 +321,22 @@ public class DateCompensatedDiscreteFourierTransform {
 	protected void standard_scan(double dang0) {
 		nfre = 1;
 		hifre = (double) numact * dang0;
-		// write(1,290) fprint,numact,dave,dsig,dvar
-		// write(1,292) dt0+tvec(nlolim),dt0+tvec(nuplim),dt0+dtzero
-		// call lognow
-		// write(1,201)
 		for (nj = 1 + npoly; nj <= numact; nj++) {
 			ff = (double) nj * dang0;
 			fft(ff);
 			// TODO: nbrake is never set to anything other than 0!!
 			if (nbrake < 0) {
 				statcomp();
-				// goto 1
 				return;
 			}
 		}
-		// goto 1
 		return;
 	}
 
 	/**
 	 * Compute a FFT.
 	 * 
-	 * @param ff
-	 *            TODO: what *is* this?
-	 * @return TODO: do we return anything? TODO: rename to dcdft()?
+	 * @param ff The frequency.
 	 */
 	protected void fft(double ff) {
 		double pp = 0;
@@ -459,16 +345,13 @@ public class DateCompensatedDiscreteFourierTransform {
 		double dd;
 
 		if (ff != 0.0)
-			pp = 1.0 / ff; // TODO: what should the default/else case be?
-		// print*,ff,pp
+			pp = 1.0 / ff; // TODO: what should the default/else pp value be?
 		dfre[nfre] = ff;
 		project();
 		// G. Foster bugfix, May 2003
-		// write(1,200) ff,pp,dfpow,damp
 		na = npoly + 1;
 		nb = na + 1;
 		dd = Math.sqrt(dcoef[na] * dcoef[na] + dcoef[nb] * dcoef[nb]);
-		// write(1,200) ff,pp,dfpow,dd // TODO: this one should be enabled
 		// System.out.println(String.format("%14.9f%10.4f%10.4f%10.4f", ff, pp,
 		// dfpow, dd));
 		collect_datapoint(ff, pp, dfpow, dd);
@@ -480,7 +363,6 @@ public class DateCompensatedDiscreteFourierTransform {
 		dlnu = ff;
 		dlper = pp;
 		dlpower = dfpow;
-		// 200 format(f14.9,3(1x,f10.4))
 	}
 
 	private void collect_datapoint(double freq, double period, double power,
@@ -507,57 +389,30 @@ public class DateCompensatedDiscreteFourierTransform {
 		}
 
 		for (ni = 0; ni <= ndim; ni++) {
-//			System.out.println(">>[ndim: " + ndim + "]<<");
-//			System.out.println(">>[ni: " + ni + "]<<");
-//			System.out.println(">>[dmat: " + dmat[ni][ni] + "]<<");
 			if (dmat[ni][ni] == 0.0) {
 				if (ni == ndim)
 					return;
-				// System.out.println("!!!!!!");
 				boolean exit_and_carry_on = false;
 				for (nj = ni + 1; nj <= ndim; nj++) {
 					if (dmat[nj][ni] != 0.0) {
 						exit_and_carry_on = true;
 						break;
 					}
-					System.out.println(">> 1");
 				}
-
-				System.out.println(">> 2");
 
 				if (!exit_and_carry_on)
 					return;
 
 				for (nk = 0; nk <= ndim; nk++) {
-					System.out.println(">> 3");
 					dmat[ni][nk] = dmat[ni][nk] + dmat[nj][nk];
 					dsol[ni][nk] = dsol[ni][nk] + dsol[nj][nk];
 				}
 			}
 
-			// System.out.println(">> 4");
-
 			dfac = dmat[ni][ni];
-//			System.out.println(String.format(">> ni: %d, dfac: %f", ni, dfac));
 			for (nj = 0; nj <= ndim; nj++) {
 				dmat[ni][nj] = dmat[ni][nj] / dfac;
 				dsol[ni][nj] = dsol[ni][nj] / dfac;
-
-				// System.out.println("dmat[ni]: ");
-				// for (double n : dmat[ni]) {
-				// System.out.print(n + " ");
-				// }
-				// System.out.println();
-				//
-				// System.out.println("dsol[ni]: ");
-				// for (double m : dsol[ni]) {
-				// System.out.print(m + " ");
-				// }
-				// System.out.println();
-				//
-				// System.out.println(String.format(
-				// "ni: %d, dmat: %10.4f, dsol: %10.4f, dfac: %10.4f", ni,
-				// dmat[ni][nj], dsol[ni][nj], dfac));
 			}
 
 			for (nj = 0; nj <= ndim; nj++) {
@@ -619,7 +474,6 @@ public class DateCompensatedDiscreteFourierTransform {
 			}
 			drad[nf] = twopi * dfre[nf] * dtscale;
 			for (nf2 = nf + 1; nf2 <= nfre; nf2++) {
-				// if (Math.abs(dfre[nf] - dfre[nf2]) < 1.d - 8) {
 				if (Math.abs(dfre[nf] - dfre[nf2]) < 1E-8) {
 					dpower = 0.0;
 					return;
@@ -657,9 +511,6 @@ public class DateCompensatedDiscreteFourierTransform {
 								+ (dpow[np] * dpow[npoly]);
 					}
 
-					// System.out.println(String.format("%d, %d, %f, %f, %f\n",
-					// np, npoly, dpow[np], dpow[npoly], dmat[np][npoly]));
-
 					dvec[np] = dvec[np] + (dx * dpow[np]);
 					n2 = npoly;
 					// ...and for products of polynomials with trig functions
@@ -668,11 +519,6 @@ public class DateCompensatedDiscreteFourierTransform {
 						dmat[np][n2 - 1] = dmat[np][n2 - 1]
 								+ (dpow[np] * dcc[nf]);
 						dmat[np][n2] = dmat[np][n2] + (dpow[np] * dss[nf]);
-
-						// System.out.println(String.format("%d, %f, %f %f %f\n",
-						// nf,
-						// dcc[nf], dss[nf], dmat[np][n2 - 1], dmat[np][n2]));
-
 					}
 				}
 
@@ -692,8 +538,6 @@ public class DateCompensatedDiscreteFourierTransform {
 						dmat[n1][n2 - 1] = dmat[n1][n2 - 1]
 								+ (dss[nf] * dcc[nf2]);
 						dmat[n1][n2] = dmat[n1][n2] + (dss[nf] * dss[nf2]);
-						// System.out.println(String.format("%d, %d, %f\n", n1,
-						// n2, dmat[n1][n2]));
 					}
 				}
 
@@ -710,11 +554,7 @@ public class DateCompensatedDiscreteFourierTransform {
 						for (nf = 1; nf <= nfre; nf++) {
 							n1 = n1 + 2;
 							dmat[n1 - 1][n2] = dmat[n1 - 1][n2] + dcc[nf];
-							// System.out.println(String.format("%d, %d, %f, %f\n",
-							// n1-1, n2, dmat[n1-1][n2], dcc[nf]));
 							dmat[n1][n2] = dmat[n1][n2] + dss[nf];
-							// System.out.println(String.format("%d, %d, %f, %f\n",
-							// n1, n2, dmat[n1][n2], dss[nf]));
 						}
 					}
 				}
@@ -724,6 +564,7 @@ public class DateCompensatedDiscreteFourierTransform {
 		// end of summation loop
 
 		// check for absent bias observers
+		// TODO: needed?
 		for (n = 1; n <= nbias; n++) {
 			if (dmat[ndim2 + n][ndim2 + n] < 1.0) {
 				// write(6,*) 'absent BIAS Obs: ',obias(n)
@@ -742,8 +583,6 @@ public class DateCompensatedDiscreteFourierTransform {
 			dvec[n1] = dvec[n1] / dweight;
 			for (n2 = n1; n2 <= ndim; n2++) {
 				dmat[n1][n2] = dmat[n1][n2] / dweight;
-				// System.out.println(String.format("%d, %d, %f", n1, n2,
-				// dmat[n1][n2]));
 			}
 		}
 
@@ -751,8 +590,6 @@ public class DateCompensatedDiscreteFourierTransform {
 		for (n1 = 1; n1 <= ndim; n1++) {
 			for (n2 = 0; n2 <= n1 - 1; n2++) {
 				dmat[n1][n2] = dmat[n2][n1];
-				// System.out.println(String.format("%d, %d, %f", n1, n2,
-				// dmat[n1][n2]));
 			}
 		}
 
@@ -763,17 +600,11 @@ public class DateCompensatedDiscreteFourierTransform {
 			dcoef[n1] = 0.0;
 			for (n2 = 0; n2 <= ndim; n2++) {
 				dcoef[n1] = dcoef[n1] + (dmat[n1][n2] * dvec[n2]);
-				// System.out.println(String.format("n1: %d, n2: %d", n1, n2));
-				// System.out.println(String.format(
-				// "dcoef: %10.4f, dmat: %10.4f, dvec: %10.4f", dcoef[n1],
-				// dmat[n1][n2], dvec[n2]));
 			}
 			damp2 = damp2 + (dcoef[n1] * dvec[n1]);
-			// System.out.println(String.format("A: damp2: %10.4f", damp2));
 		}
 
 		damp2 = damp2 - (dave * dave);
-		// System.out.println(String.format("B: damp2: %10.4f", damp2));
 		if (damp2 < 0.0)
 			damp2 = 0.0;
 		if (ndim > 0) {
@@ -785,19 +616,11 @@ public class DateCompensatedDiscreteFourierTransform {
 		// compute Fourier power, amplitude squared
 
 		dfpow = (double) (numact - 1) * (damp2 - dfouramp2);
-		// System.out.println(String.format(
-		// "1: dfpow: %8.4f, damp2: %8.4f, dfouramp2: %8s.4f", dfpow,
-		// damp2, dfouramp2));
 		dfpow = dfpow / (dvar - dfouramp2) / 2.0;
-		// System.out.println(String.format("2: dfpow: %10.4f, dvar: %10.4f",
-		// dfpow, dvar));
 		damp = 2.0 * (damp2 - dfouramp2);
-		// System.out.println(String.format("3: damp: %10.4f", damp));
 		if (damp < 0.0)
 			damp = 0.0;
 		damp = Math.sqrt(damp);
-		// System.out.println(String.format("4: damp: %10.4f", damp));
-		// System.out.println();
 	}
 
 	// -------------------------------------------------------------------------------
