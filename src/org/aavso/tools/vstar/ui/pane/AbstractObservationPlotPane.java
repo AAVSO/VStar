bfa0d0f2939e0d4a12d0a5121d87a4f693c878ae
david_benn <4115628+dbenn@users.noreply.github.com>
2010-04-18 02:31:39 +0000

    The change at to AbstractObservationPlotPane to use HQ uncertainty when uncertainty is not supplied caused null pointer exceptions when HQ uncertainty was not present (null). Fixed that at 476.

--- src/org/aavso/tools/vstar/ui/pane/AbstractObservationPlotPane.java
+++ src/org/aavso/tools/vstar/ui/pane/AbstractObservationPlotPane.java
@@ -442,8 +442,8 @@ abstract public class AbstractObservationPlotPane<T extends ObservationPlotModel
 				for (ValidObservation ob : obs) {
 					double mag = ob.getMagnitude().getMagValue();
 					double uncert = ob.getMagnitude().getUncertainty();
-					//here if getUncertainty is null then get HQ
-					if(uncert == 0.0){
+					// If uncertainty not given, get HQ uncertainty if present.
+					if (uncert == 0.0 && ob.getHqUncertainty() != null){
 						uncert = ob.getHqUncertainty();
 					}
 
