58fed639533f9c0f371f051e0400e1dc62a1615c
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2010-04-25 15:21:27 +0000

    Increased the default size of the Error column in the invalid observation table. See also tracker 2944557.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:480MP";
+	private static String REVISION = "370:481MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
