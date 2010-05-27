b4d3551f299cca167cbd3d294145559b43134aff
david_benn <4115628+dbenn@users.noreply.github.com>
2010-05-27 12:51:13 +0000

    Removed "internal use only" from a few comment code; various other small changes; change log updates. See tracker 2909469.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:505MP";
+	private static String REVISION = "370:508MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
