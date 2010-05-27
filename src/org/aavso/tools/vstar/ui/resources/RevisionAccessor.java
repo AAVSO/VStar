af9394247af3d4a5c31b3d487d5bda463f607294
david_benn <4115628+dbenn@users.noreply.github.com>
2010-05-27 14:44:07 +0000

    Implemented fix for tracker 3007948: observation change notifier in Mediator is re-allocated; simply removing listeners from the notifier leads to repeatable concurrent modification exceptions. Other code cleanup and change log updated in preparation for a release of 2.0 Beta.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:508MP";
+	private static String REVISION = "370:509MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
