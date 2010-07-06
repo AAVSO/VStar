dcab978d5d2f1473409e38545ba93d637124d511
david_benn <4115628+dbenn@users.noreply.github.com>
2010-07-06 13:40:32 +0000

    Updated help contents re: observer code retrieval.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:532MP";
+	private static String REVISION = "370:533MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
