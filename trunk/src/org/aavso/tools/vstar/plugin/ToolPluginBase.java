6e27c4a874fb82c9a3e92f2bbfb56877eb044c9f
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2010-08-15 04:17:43 +0000

    Added Javadoc generation to Ant files and made some key comment blocks more Javadoc friendly.

--- trunk/src/org/aavso/tools/vstar/plugin/ToolPluginBase.java
+++ trunk/src/org/aavso/tools/vstar/plugin/ToolPluginBase.java
@@ -22,20 +22,23 @@ import java.util.List;
 import org.aavso.tools.vstar.data.ValidObservation;
 
 /**
- * This is the base class for all arbitrary VStar "tool" plugins, where "tool"
+ * <p>This is the base class for all arbitrary VStar "tool" plugins, where "tool"
  * can be anything that takes as input a list of valid observations and does any
- * arbitrary thing with them. Tool plugins will appear in VStar's Tools menu
- * when its jar file is placed into the VStar plugins directory.
+ * arbitrary thing with them.</p>
+ * 
+ * <p>Tool plugins will appear in VStar's Tools menu when its jar file is placed into 
+ * the vstar_plugins directory.</p>
+ * 
+ * @see org.aavso.tools.vstar.plugin.PluginBase
+ * @see org.aavso.tools.vstar.ui.mediator.Mediator#invokeToolWithCurrentObservations(org.aavso.tools.vstar.plugin.ToolPluginBase)
  */
 abstract public class ToolPluginBase implements PluginBase {
 
 	/**
-	 * Given a list of valid observations, do *something*. :)
+	 * Given a list of valid observations, do something, <em>anything</em> in fact. :)
 	 * 
 	 * @param obs
-	 *            A list of valid observations. The intention is that this
-	 *            should be *all* currently loaded valid observations.
-	 * @see org.aavso.tools.vstar.ui.mediator.Mediator#invokeToolWithCurrentObservations(org.aavso.tools.vstar.plugin.ToolPluginBase)
+	 *            A list of currently loaded observations.
 	 */
 	abstract public void invoke(List<ValidObservation> obs);
 }
