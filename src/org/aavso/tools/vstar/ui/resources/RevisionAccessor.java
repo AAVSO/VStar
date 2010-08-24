d8c7c821b40c8369b0aa69b76b0f19ce62a4230b
david_benn <4115628+dbenn@users.noreply.github.com>
2010-08-24 13:43:29 +0000

    Rebuilt vstar.jar, ran unit tests, regenerated javadoc, updated change log again, in preparation for 2.3 Beta release.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:566MP";
+	private static String REVISION = "370:567MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
