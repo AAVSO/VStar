ab11aadfbe57ee66813d91f514a2d7f970d36d67
david_benn <4115628+dbenn@users.noreply.github.com>
2010-11-10 13:43:15 +0000

    Added invalid observation handling to FITS plugin classes.

--- plugin/src/org/aavso/tools/vstar/external/plugin/KeplerFITSObservationSource.java
+++ plugin/src/org/aavso/tools/vstar/external/plugin/KeplerFITSObservationSource.java
@@ -1,4 +1,5 @@
 package org.aavso.tools.vstar.external.plugin;
+
 /**
  * VStar: a statistical analysis tool for variable star data.
  * Copyright (C) 2009  AAVSO (http://www.aavso.org/)
@@ -23,6 +24,7 @@ import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
 
 import org.aavso.tools.vstar.data.DateInfo;
+import org.aavso.tools.vstar.data.InvalidObservation;
 import org.aavso.tools.vstar.data.Magnitude;
 import org.aavso.tools.vstar.data.SeriesType;
 import org.aavso.tools.vstar.data.ValidObservation;
@@ -32,7 +34,8 @@ import org.aavso.tools.vstar.plugin.InputType;
 import org.aavso.tools.vstar.plugin.ObservationSourcePluginBase;
 
 /**
- * A Kepler FITS file observation source plug-inputStream that uses the Topcat FITS library.
+ * A Kepler FITS file observation source plug-inputStream that uses the Topcat
+ * FITS library.
  */
 public class KeplerFITSObservationSource extends ObservationSourcePluginBase {
 
@@ -91,46 +94,77 @@ public class KeplerFITSObservationSource extends ObservationSourcePluginBase {
 					BinaryTableHDU tableHDU = (BinaryTableHDU) hdu;
 
 					for (int row = 0; row < tableHDU.getNRows(); row++) {
-						double barytime = ((double[]) tableHDU.getElement(row, 0))[0];
-						float timcorr = ((float[]) tableHDU.getElement(row, 1))[0];
-						int cadence_number = ((int[]) tableHDU.getElement(row, 2))[0];
-						double ap_cent_row = ((double[]) tableHDU.getElement(row, 3))[0];
-						float ap_cent_r_err = ((float[]) tableHDU.getElement(row, 4))[0];
-						double ap_cent_col = ((double[]) tableHDU.getElement(row, 5))[0];
-						float ap_cent_c_err = ((float[]) tableHDU.getElement(row, 6))[0];
-						float ap_raw_flux = ((float[]) tableHDU.getElement(row, 7))[0];
-						float ap_raw_err = ((float[]) tableHDU.getElement(row, 8))[0];
-						float ap_corr_flux = ((float[]) tableHDU.getElement(row, 9))[0];
-                                                float ap_corr_err = ((float[]) tableHDU.getElement(row, 10))[0];
-						float ap_ins_flux = ((float[]) tableHDU.getElement(row, 11))[0];
-                                                float ap_ins_err = ((float[]) tableHDU.getElement(row, 12))[0];
-						float dia_raw_flux = ((float[]) tableHDU.getElement(row, 13))[0];
-                                                float dia_raw_err = ((float[]) tableHDU.getElement(row, 14))[0];
-						float dia_corr_flux = ((float[]) tableHDU.getElement(row, 15))[0];
-                                                float dia_corr_err = ((float[]) tableHDU.getElement(row, 16))[0];
-						float dia_ins_flux = ((float[]) tableHDU.getElement(row, 17))[0];
-                                                float dia_ins_err = ((float[]) tableHDU.getElement(row, 18))[0];
+						try {
+							double barytime = ((double[]) tableHDU.getElement(
+									row, 0))[0];
+							float timcorr = ((float[]) tableHDU.getElement(row,
+									1))[0];
+							int cadence_number = ((int[]) tableHDU.getElement(
+									row, 2))[0];
+							double ap_cent_row = ((double[]) tableHDU
+									.getElement(row, 3))[0];
+							float ap_cent_r_err = ((float[]) tableHDU
+									.getElement(row, 4))[0];
+							double ap_cent_col = ((double[]) tableHDU
+									.getElement(row, 5))[0];
+							float ap_cent_c_err = ((float[]) tableHDU
+									.getElement(row, 6))[0];
+							float ap_raw_flux = ((float[]) tableHDU.getElement(
+									row, 7))[0];
+							float ap_raw_err = ((float[]) tableHDU.getElement(
+									row, 8))[0];
+							float ap_corr_flux = ((float[]) tableHDU
+									.getElement(row, 9))[0];
+							float ap_corr_err = ((float[]) tableHDU.getElement(
+									row, 10))[0];
+							float ap_ins_flux = ((float[]) tableHDU.getElement(
+									row, 11))[0];
+							float ap_ins_err = ((float[]) tableHDU.getElement(
+									row, 12))[0];
+							float dia_raw_flux = ((float[]) tableHDU
+									.getElement(row, 13))[0];
+							float dia_raw_err = ((float[]) tableHDU.getElement(
+									row, 14))[0];
+							float dia_corr_flux = ((float[]) tableHDU
+									.getElement(row, 15))[0];
+							float dia_corr_err = ((float[]) tableHDU
+									.getElement(row, 16))[0];
+							float dia_ins_flux = ((float[]) tableHDU
+									.getElement(row, 17))[0];
+							float dia_ins_err = ((float[]) tableHDU.getElement(
+									row, 18))[0];
 
-						// For non-infinite magnitude fluxes...
-                        if (!Float.isInfinite(ap_corr_flux) && !Float.isInfinite(ap_corr_err)) { 
-                                                
-							double hjd = barytime + 2400000.5;
-							double mag = 15.0 - 2.5 * Math.log(ap_corr_flux)
-									/ Math.log(10.0);
-							double magErr = 1.086 * ap_corr_err / ap_corr_flux;
+							// For non-infinite magnitude fluxes...
+							if (!Float.isInfinite(ap_corr_flux)
+									&& !Float.isInfinite(ap_corr_err)) {
 
-							if (magErr < minMagErr) {
-								minMagErr = magErr;
-							} else if (magErr > maxMagErr) {
-								maxMagErr = magErr;
+								double hjd = barytime + 2400000.5;
+								double mag = 15.0 - 2.5
+										* Math.log(ap_corr_flux)
+										/ Math.log(10.0);
+								double magErr = 1.086 * ap_corr_err
+										/ ap_corr_flux;
+
+								if (magErr < minMagErr) {
+									minMagErr = magErr;
+								} else if (magErr > maxMagErr) {
+									maxMagErr = magErr;
+								}
+
+								ValidObservation ob = new ValidObservation();
+								ob.setDateInfo(new DateInfo(hjd));
+								ob.setMagnitude(new Magnitude(mag, magErr));
+								ob.setBand(SeriesType.Unspecified);
+								ob.setRecordNumber(row);
+								collectObservation(ob);
 							}
-
-							ValidObservation ob = new ValidObservation();
-							ob.setDateInfo(new DateInfo(hjd));
-							ob.setMagnitude(new Magnitude(mag, magErr));
-							ob.setBand(SeriesType.Unspecified);
+						} catch (Exception e) {
+							String input = tableHDU.getRow(row).toString();
+							String error = e.getLocalizedMessage();
+							InvalidObservation ob = new InvalidObservation(
+									input, error);
 							ob.setRecordNumber(row);
-							collectObservation(ob);
+							addInvalidObservation(ob);
 						}
 					}
 				}
