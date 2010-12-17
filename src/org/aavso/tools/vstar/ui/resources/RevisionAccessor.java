523e7784484c88bcd990887cf902454ec9a9b1ad
david_benn <4115628+dbenn@users.noreply.github.com>
2010-12-17 13:55:04 +0000

    Fix to help doc and update of jar.

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "370:662MP";
+	private static String REVISION = "370:663MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
