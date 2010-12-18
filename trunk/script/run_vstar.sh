10ed07fca2a18dfe5aa4c09011c65109b1a29740
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2010-12-18 01:48:47 +0000

    Removing old 2.10 tag

--- trunk/script/run_vstar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Run VStar with the same VM configuration as via JNLP.
-#
-# You need to set VSTAR_HOME to the VStar root directory,
-# e.g. under Linux/Mac OS X: export VSTAR_HOME=/home/david/vstar
-
-java -Xms25m -Xmx500m -jar $VSTAR_HOME/dist/vstar.jar
