85edcfe38000f348c3723ca64b06debc8fda8f8d
mu301 <mu301@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2011-08-08 15:24:04 +0000

    Minor changes to DOS batch script per tracker 3383261

--- vstar.bat
+++ vstar.bat
@@ -3,12 +3,11 @@
 :: VSTAR_HOME needs to be set to the VStar root directory,
 :: e.g. set VSTAR_HOME=C:\vstar
 :: If not set, the script assumes the current directory is the
-:: VStar root directory. If this is not the case, the script will
-:: fail.
+:: directory that the script is running from.
 
-if "%VSTAR_HOME%" !==! "" GOTO RUN
+if not "%VSTAR_HOME%" == "" GOTO RUN
 
-set VSTAR_HOME=.
+set VSTAR_HOME=%~dp0
 
 :RUN 
-java -Xms25m -Xmx500m -jar %VSTAR_HOME%\dist\vstar.jar
+java -Xms25m -Xmx500m -jar "%VSTAR_HOME%\dist\vstar.jar"
