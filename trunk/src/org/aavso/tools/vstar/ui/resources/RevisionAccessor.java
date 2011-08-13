29180dc77e821e680cb78766b621d82842b8fed9
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2011-08-13 10:19:36 +0000

    Removed run_vstar.* zipfileset entry and added filemode=755 attribute for vstar shell script; updated ReadMe.txt re: the new official run scripts. See tracker 3383261.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "724:798MP";
+	private static String REVISION = "724:807MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
