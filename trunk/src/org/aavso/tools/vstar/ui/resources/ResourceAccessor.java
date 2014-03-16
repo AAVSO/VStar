0ef7e71e792183dc5212305f646c314ec5792d01
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2014-03-16 12:30:18 +0000

    Updated change log, dist jar, version in preparation for release.

--- trunk/src/org/aavso/tools/vstar/ui/resources/ResourceAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/ResourceAccessor.java
@@ -137,7 +137,7 @@ public class ResourceAccessor {
 	// ** Version info. **
 
 	public static String getVersionString() {
-		return "2.15.5";
+		return "2.15.6";
 	}
 
 	public static String getRevNum() {
