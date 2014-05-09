9e426e93c853f988ab7fb514e1023b924ec50057
david_benn <4115628+dbenn@users.noreply.github.com>
2014-05-09 15:16:17 +0000

    Final commits in preparation for 2.16.0 release

--- src/org/aavso/tools/vstar/ui/resources/ResourceAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/ResourceAccessor.java
@@ -137,7 +137,7 @@ public class ResourceAccessor {
 	// ** Version info. **
 
 	public static String getVersionString() {
-		return "2.15.6";
+		return "2.16.0";
 	}
 
 	public static String getRevNum() {
