0d785dce1fe91ade042c24e79b88a952ea00c551
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2014-08-08 15:14:44 +0000

    APASS/BSM plug-ins set as HJD observation sources. Updates to user manual. Preparing for 2.16.3 release.

--- trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "955:1169MP";
+	private static String REVISION = "955:1176MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
