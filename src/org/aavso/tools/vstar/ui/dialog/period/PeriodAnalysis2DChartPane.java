7016c3a4682a13e1262d870871dca6594e49a0f1
david_benn <4115628+dbenn@users.noreply.github.com>
2015-06-17 23:48:29 +0000

    Generalised domain and range value retrieval

--- src/org/aavso/tools/vstar/ui/dialog/period/PeriodAnalysis2DChartPane.java
+++ src/org/aavso/tools/vstar/ui/dialog/period/PeriodAnalysis2DChartPane.java
@@ -37,7 +37,6 @@ import org.aavso.tools.vstar.ui.model.plot.PeriodAnalysis2DPlotModel;
 import org.aavso.tools.vstar.util.IStartAndCleanup;
 import org.aavso.tools.vstar.util.locale.LocaleProps;
 import org.aavso.tools.vstar.util.notification.Listener;
-import org.aavso.tools.vstar.util.period.PeriodAnalysisCoordinateType;
 import org.aavso.tools.vstar.util.period.dcdft.PeriodAnalysisDataPoint;
 import org.jfree.chart.ChartMouseEvent;
 import org.jfree.chart.ChartMouseListener;
@@ -245,26 +244,22 @@ public class PeriodAnalysis2DChartPane extends JPanel implements
 				if (info.getSource() != parent) {
 					double x = Double.NaN;
 					double y = Double.NaN;
-
-					// TODO: generalise to simply use domain type
 					
-					if (model.getDomainType() == PeriodAnalysisCoordinateType.FREQUENCY) {
-						x = info.getDataPoint().getFrequency();
-					} else if (model.getDomainType() == PeriodAnalysisCoordinateType.PERIOD) {
-						x = info.getDataPoint().getPeriod();
-					} else {
+//					if (model.getDomainType() == PeriodAnalysisCoordinateType.FREQUENCY) {
+//						x = info.getDataPoint().getFrequency();
+//					} else if (model.getDomainType() == PeriodAnalysisCoordinateType.PERIOD) {
+//						x = info.getDataPoint().getPeriod();
+//					} else {
 						x = info.getDataPoint().getValue(model.getDomainType());
-					}
+//					}
 
-					// TODO: generalise to simply use range type
-
-					if (model.getRangeType() == PeriodAnalysisCoordinateType.POWER) {
-						y = info.getDataPoint().getPower();
-					} else if (model.getRangeType() == PeriodAnalysisCoordinateType.AMPLITUDE) {
-						y = info.getDataPoint().getAmplitude();
-					} else {
+//					if (model.getRangeType() == PeriodAnalysisCoordinateType.POWER) {
+//						y = info.getDataPoint().getPower();
+//					} else if (model.getRangeType() == PeriodAnalysisCoordinateType.AMPLITUDE) {
+//						y = info.getDataPoint().getAmplitude();
+//					} else {
 						y = info.getDataPoint().getValue(model.getRangeType());
-					}
+//					}
 
 					chart.getXYPlot().setDomainCrosshairValue(x);
 					chart.getXYPlot().setRangeCrosshairValue(y);
