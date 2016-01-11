ededcdfce803afe0157e9bdeba0048b77b37ac0b
david_benn <david_benn@ca1ac435-7b94-4a5d-8ecd-2a1874c1a5f0>
2016-01-11 11:51:52 +0000

    Updated version, dist before 2.16.10 release

--- trunk/src/org/aavso/tools/vstar/ui/resources/ResourceAccessor.java
+++ trunk/src/org/aavso/tools/vstar/ui/resources/ResourceAccessor.java
@@ -137,7 +137,7 @@ public class ResourceAccessor {
 	// ** Version info. **
 
 	public static String getVersionString() {
-		return "2.16.10-RC2";
+		return "2.16.10";
 	}
 
 	public static String getRevNum() {
