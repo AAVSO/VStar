536aca2c2cba76a540d177a2874a4fd2aa5e7f13
david_benn <4115628+dbenn@users.noreply.github.com>
2019-07-06 11:55:50 +0000

    Observation transformation support added to plugin loader

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "1617MP";
+	private static String REVISION = "1617:1630MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
