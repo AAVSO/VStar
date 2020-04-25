fb134db3402bf2edd7f28e90343149213445a8c1
david_benn <4115628+dbenn@users.noreply.github.com>
2020-04-25 03:21:09 +0000

    Property based unit tests for phase calcs and VeLa.

---
+++ test/org/aavso/tools/vstar/util/stats/PhaseCalcsPropertyBasedTest.java
@@ -0,0 +1,139 @@
+/**
+ * VStar: a statistical analysis tool for variable star data.
+ * Copyright (C) 2009  AAVSO (http://www.aavso.org/)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+package org.aavso.tools.vstar.util.stats;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aavso.tools.vstar.data.DateInfo;
+import org.aavso.tools.vstar.data.Magnitude;
+import org.aavso.tools.vstar.data.MagnitudeModifier;
+import org.aavso.tools.vstar.data.ValidObservation;
+import org.aavso.tools.vstar.util.stats.epoch.AlphaOmegaMeanJDEpochStrategy;
+import org.aavso.tools.vstar.util.stats.epoch.MaxMagEpochStrategy;
+import org.aavso.tools.vstar.util.stats.epoch.MinMagEpochStrategy;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+/**
+ * Phase calculation UTs.
+ */
+@RunWith(JUnitQuickcheck.class)
+public class PhaseCalcsPropertyBasedTest {
+
+	private final static double[] mags1 = { 3, 3.5, 3.6, 3.2, 3.1 };
+	private final static double[] jds1 = { 2450001.5, 2450002, 2450003.5,
+			2450004.5, 2450005 };
+
+	private final static double DELTA = 0.001;
+
+	// Valid
+
+	@Test
+	public void testStandardPhase1() {
+		double phase = PhaseCalcs.calcStandardPhase(2450004, 2450002, 10);
+		assertEquals(0.2, phase, DELTA);
+	}
+
+	@Test
+	public void testStandardPhase2() {
+		double phase = PhaseCalcs.calcStandardPhase(2450002, 2450004, 10);
+		assertEquals(0.8, phase, DELTA);
+	}
+
+	@Test
+	public void testDetermineEpochAlphaAndOmegaMeanJD() {
+		List<ValidObservation> observations = populateObservations(mags1, jds1);
+		double epoch = new AlphaOmegaMeanJDEpochStrategy()
+				.determineEpoch(observations);
+		assertEquals(2450003.25, epoch, DELTA);
+	}
+
+	@Test
+	public void testDetermineMinMagJD() {
+		List<ValidObservation> observations = populateObservations(mags1, jds1);
+		double epoch = new MinMagEpochStrategy().determineEpoch(observations);
+		assertEquals(2450001.5, epoch, DELTA);
+	}
+
+	@Test
+	public void testDetermineMaxMagJD() {
+		List<ValidObservation> observations = populateObservations(mags1, jds1);
+		double epoch = new MaxMagEpochStrategy().determineEpoch(observations);
+		assertEquals(2450003.5, epoch, DELTA);
+	}
+
+	@Test
+	public void testPhases1() {
+		List<ValidObservation> observations = populateObservations(mags1, jds1);
+		double epoch = 2450003.25;
+		double period = 10;
+		PhaseCalcs.setPhases(observations, epoch, period);
+
+		// Standard phases: 0.82499999999999996, 0.875, 0.025000000000000001,
+		// 0.125, 0.17499999999999999
+
+		// Check two standard phases, one that had an intermediate negative
+		// value (standard phase of 0.875 in element 1).
+		assertEquals(0.875, observations.get(1).getStandardPhase(), DELTA);
+		assertEquals(-0.125, observations.get(1).getPreviousCyclePhase(), DELTA);
+
+		assertEquals(0.125, observations.get(3).getStandardPhase(), DELTA);
+		assertEquals(-0.875, observations.get(3).getPreviousCyclePhase(), DELTA);
+	}
+
+	// First property-based test in VStar!
+	// This says that given valid inputs, the phase should always be in the
+	// range 0..1 inclusive.
+	@Property(trials=1000)
+	public void phaseInRange(@InRange(minDouble = 0) double jd,
+			@InRange(minDouble = 0) double epoch, double period) {
+		assumeTrue(period > 0);
+		double phase = PhaseCalcs.calcStandardPhase(jd, epoch, period);
+		assertTrue(phase >= 0 && phase <= 1);
+	}
+
+	// Helpers
+
+	// Populates and returns a list of valid observations with supplied
+	// magnitude values and JD values.
+	private List<ValidObservation> populateObservations(double[] mags,
+			double[] jds) {
+		assertTrue(mags.length == jds.length);
+
+		List<ValidObservation> observations = new ArrayList<ValidObservation>();
+
+		for (int i = 0; i < mags.length; i++) {
+			ValidObservation obs = new ValidObservation();
+			obs.setMagnitude(new Magnitude(mags[i], MagnitudeModifier.NO_DELTA,
+					false));
+			obs.setDateInfo(new DateInfo(jds[i]));
+			observations.add(obs);
+		}
+
+		return observations;
+	}
+}
