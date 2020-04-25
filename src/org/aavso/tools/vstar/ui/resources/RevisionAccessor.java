8d22e11cea9be311e2d38544bccbce9db5d25c8a
david_benn <4115628+dbenn@users.noreply.github.com>
2020-04-25 03:16:50 +0000

    Updated resource/revision files

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "1617:1756MP";
+	private static String REVISION = "1617:1759MP";
 
 	private static final Pattern revNumPat = Pattern
 			.compile("^\\d+:(\\d+).*$");
