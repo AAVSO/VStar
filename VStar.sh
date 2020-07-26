8a9fd007e611bdb82dfe735b85196223dda85b7c
David Benn <dbenn@computer.org>
2020-07-26 22:15:54 +0930

    issue-38: setting max memory dynamically for 32 vs 64 bit OS

--- VStar.sh
+++ VStar.sh
@@ -4,6 +4,14 @@
 
 APP_DIR=$(dirname "$0")
 
+# 32 or 64 bit?
+VER=`uname -a | grep _64`
+
+if [ "$VER" != "" ]; then
+    MAX_MEM=32g
+else
+    MAX_MEM=1500mb
+fi
+
 java -splash:"$APP_DIR/extlib/vstaricon.png" \
-     -Xms800m -Xmx1500m -jar "$APP_DIR/dist/vstar.jar" $*
-     
+     -Xms800m -Xmx${MAX_MEM} -jar "$APP_DIR/dist/vstar.jar" $*
