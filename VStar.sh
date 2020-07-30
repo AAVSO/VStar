7bc24a192addb7ea2939bc65de6922000e078a04
David Benn <dbenn@computer.org>
2020-07-30 23:22:42 +0930

    issue-38: setting max heap to half available memory

--- VStar.sh
+++ VStar.sh
@@ -8,12 +8,21 @@ APP_DIR=$(dirname "$0")
 VER=`uname -a | grep _64`
 
 if [ "$VER" != "" ]; then
-    # 64 bit, so determine half of available memory
-    HALF_MEM=$(perl -e "print `sysctl -n hw.memsize` / (1024*1024*1024) / 2;")
+    # 64 bit, so determine half of available memory for Mac OS X or Linux
+    if [ `echo $OSTYPE | grep darwin` != "" ]; then
+        # Mac OS X...
+        HALF_MEM=$(perl -e "print int(`sysctl -n hw.memsize` / (1024*1024*1024) / 2);")
+    else
+        # ...otherwise, assume Linux/Unix
+        HALF_MEM=$(perl -e "print int(`free -h | awk '/^Mem:/{print $2}' | sed 's/G//'` / 2);")
+    fi
+    
     MAX_MEM=${HALF_MEM}g
 else
     MAX_MEM=1500mb
 fi
+
+#echo $MAX_MEM
 
 java -splash:"$APP_DIR/extlib/vstaricon.png" \
      -Xms800m -Xmx${MAX_MEM} -jar "$APP_DIR/dist/vstar.jar" $*
