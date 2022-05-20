5f74c51f1d43286b50fdf0172574b7778b21429a
David Benn <dbenn@computer.org>
2022-05-20 18:22:03 +0930

    #256: improved FWHM outputs including error value

--- src/org/aavso/tools/vstar/util/model/PeriodAnalysisDerivedMultiPeriodicModel.java
+++ src/org/aavso/tools/vstar/util/model/PeriodAnalysisDerivedMultiPeriodicModel.java
@@ -166,15 +166,16 @@ public class PeriodAnalysisDerivedMultiPeriodicModel implements IModel {
 		if (strRepr == null) {
 			strRepr = String.format("For frequency %s, period %s, power %s, semi-amplitude %s:\n\n",
 					NumericPrecisionPrefs.formatOther(freq), NumericPrecisionPrefs.formatOther(period),
-					NumericPrecisionPrefs.formatOther(power),
-					NumericPrecisionPrefs.formatOther(semiAmplitude));
+					NumericPrecisionPrefs.formatOther(power), NumericPrecisionPrefs.formatOther(semiAmplitude));
 
 			// Full Width Half Maximum
 			try {
 				Pair<Double, Double> fwhm = fwhm();
 				strRepr += "  FWHM for frequency:\n";
 				strRepr += "    Lower bound: " + NumericPrecisionPrefs.formatOther(fwhm.first) + "\n";
-				strRepr += "    Upper bound: " + NumericPrecisionPrefs.formatOther(fwhm.second) + "\n\n";
+				strRepr += "    Upper bound: " + NumericPrecisionPrefs.formatOther(fwhm.second) + "\n";
+				double fwhmError = (fwhm.second - fwhm.first) / 2;
+				strRepr += "    Resulting error: " + NumericPrecisionPrefs.formatOther(fwhmError) + "\n\n";
 			} catch (AlgorithmError e) {
 				// don't report this uncertainty
 				strRepr = "";
