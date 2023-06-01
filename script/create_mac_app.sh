dff2a4fe751fc4b4d1dad38f2072c219956d0ae1
David Benn <dbenn@computer.org>
2023-06-01 23:49:25 +0930

    #331: only including the required extlib files

--- script/create_mac_app.sh
+++ script/create_mac_app.sh
@@ -2,7 +2,17 @@
 #
 # Create Mac OSX application.
 
-cp -rf install-files/mac/VStar.app .
+cp -r install-files/mac/VStar.app .
+
 cp VStar.sh VStar.app/Contents/MacOS
+
 cp -r dist VStar.app/Contents/MacOS
-cp -r extlib VStar.app/Contents/MacOS
+
+mkdir VStar.app/Contents/MacOS/extlib
+cp extlib/commons-math-2.2.jar VStar.app/Contents/MacOS/extlib
+cp extlib/javacsv.jar VStar.app/Contents/MacOS/extlib
+cp extlib/jfreechart-1.5.2.jar VStar.app/Contents/MacOS/extlib
+cp extlib/jmathplot.jar VStar.app/Contents/MacOS/extlib
+cp extlib/antlr-runtime-4.5.3.jar VStar.app/Contents/MacOS/extlib
+cp extlib/nico-camargo-toolbar-icons-4.0.jar VStar.app/Contents/MacOS/extlib
+cp extlib/vstaricon.png VStar.app/Contents/MacOS/extlib
