8f6e0f26254bc7b69c1a20956e3b1ef8682d985a
mpyat2 <mpyat2@gmail.com>
2023-10-01 12:32:03 +0300

    build-win-installer.xml: Version number set to 2.23.0

--- src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
+++ src/org/aavso/tools/vstar/ui/resources/RevisionAccessor.java
@@ -26,8 +26,8 @@ import java.util.regex.Pattern;
 
 public class RevisionAccessor {
 
-	private static String REVISION = "1175382";
-	private static String BUILD_TIME = "2023-09-06 22:03";
+	private static String REVISION = "08f9f87c";
+	private static String BUILD_TIME = "2023-10-01 12:27";
 
 	/**
 	 * Get the latest git revision
