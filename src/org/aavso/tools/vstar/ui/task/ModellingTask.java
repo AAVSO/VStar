33c97f2aa44f29d0c417601483129d96f00680e5
David Benn <dbenn@computer.org>
2025-04-18 13:24:24 +0930

    #485: addressed the problem seen when cancelling VeLa dialog from null VeLa string, and preventing model series being created

--- src/org/aavso/tools/vstar/ui/task/ModellingTask.java
+++ src/org/aavso/tools/vstar/ui/task/ModellingTask.java
@@ -34,96 +34,89 @@ import org.aavso.tools.vstar.util.stats.PhaseCalcs;
  */
 public class ModellingTask extends SwingWorker<Void, Void> {
 
-	private String error;
-	private IModel model;
+    private String error;
+    private IModel model;
 
-	private Listener<StopRequestMessage> stopListener;
+    private Listener<StopRequestMessage> stopListener;
 
-	/**
-	 * Constructor
-	 * 
-	 * @param model
-	 *            The model algorithm to execute.
-	 */
-	public ModellingTask(IModel model) {
-		this.error = null;
-		this.model = model;
+    /**
+     * Constructor
+     * 
+     * @param model The model algorithm to execute.
+     */
+    public ModellingTask(IModel model) {
+        this.error = null;
+        this.model = model;
 
-		stopListener = createStopRequestListener();
-	}
+        stopListener = createStopRequestListener();
+    }
 
-	/**
-	 * @see javax.swing.SwingWorker#doInBackground()
-	 */
-	protected Void doInBackground() throws Exception {
+    /**
+     * @see javax.swing.SwingWorker#doInBackground()
+     */
+    protected Void doInBackground() throws Exception {
 
-		Mediator.getInstance().getStopRequestNotifier().addListener(
-				stopListener);
+        Mediator.getInstance().getStopRequestNotifier().addListener(stopListener);
 
-        Mediator.getInstance().getProgressNotifier().notifyListeners(
-                ProgressInfo.START_PROGRESS);
+        Mediator.getInstance().getProgressNotifier().notifyListeners(ProgressInfo.START_PROGRESS);
 
-		Mediator.getUI().getStatusPane().setMessage(
-				"Performing " + model.getKind() + "...");
-		try {
-			model.execute();
-			// Is there a phase plot in effect?
-			// If so, set the model's phase values accordingly.
-			if (Mediator.getInstance().getDocumentManager().phasePlotExists()) {
-				double epoch = Mediator.getInstance().getDocumentManager().getEpoch();
-				double period = Mediator.getInstance().getDocumentManager().getPeriod();
-				PhaseCalcs.setPhases(model.getFit(), epoch, period);
-				PhaseCalcs.setPhases(model.getResiduals(), epoch, period);
-			}
-		} catch (Throwable t) {
-			error = t.getLocalizedMessage();
-		} finally {
-			Mediator.getInstance().getStopRequestNotifier()
-					.removeListenerIfWilling(stopListener);
-		}
+        Mediator.getUI().getStatusPane().setMessage("Performing " + model.getKind() + "...");
+        try {
+            model.execute();
+            if (!model.getFit().isEmpty()) {
+                // Is there a phase plot in effect?
+                // If so, set the model's phase values accordingly.
+                if (Mediator.getInstance().getDocumentManager().phasePlotExists()) {
+                    double epoch = Mediator.getInstance().getDocumentManager().getEpoch();
+                    double period = Mediator.getInstance().getDocumentManager().getPeriod();
+                    PhaseCalcs.setPhases(model.getFit(), epoch, period);
+                    PhaseCalcs.setPhases(model.getResiduals(), epoch, period);
+                }
+            }
+        } catch (Throwable t) {
+            error = t.getLocalizedMessage();
+        } finally {
+            Mediator.getInstance().getStopRequestNotifier().removeListenerIfWilling(stopListener);
+        }
 
-		Mediator.getUI().getStatusPane().setMessage("");
+        Mediator.getUI().getStatusPane().setMessage("");
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Executed in event dispatching thread.
-	 */
-	public void done() {
-	    if (error != null) {
-	        MessageBox.showErrorDialog(model.getKind() + " Error", error);
-	    } else if (!isCancelled()) {
-			ModelSelectionMessage selectionMsg = new ModelSelectionMessage(
-					this, model);
-			Mediator.getInstance().getModelSelectionNofitier().notifyListeners(
-					selectionMsg);
+    /**
+     * Executed in event dispatching thread.
+     */
+    public void done() {
+        if (!model.getFit().isEmpty()) {
+            if (error != null) {
+                MessageBox.showErrorDialog(model.getKind() + " Error", error);
+            } else if (!isCancelled()) {
+                ModelSelectionMessage selectionMsg = new ModelSelectionMessage(this, model);
+                Mediator.getInstance().getModelSelectionNofitier().notifyListeners(selectionMsg);
 
-			ModelCreationMessage creationMsg = new ModelCreationMessage(this,
-					model);
-			Mediator.getInstance().getModelCreationNotifier().notifyListeners(
-					creationMsg);
-		}
+                ModelCreationMessage creationMsg = new ModelCreationMessage(this, model);
+                Mediator.getInstance().getModelCreationNotifier().notifyListeners(creationMsg);
+            }
+        }
 
-		Mediator.getInstance().getProgressNotifier().notifyListeners(
-				ProgressInfo.COMPLETE_PROGRESS);
+        Mediator.getInstance().getProgressNotifier().notifyListeners(ProgressInfo.COMPLETE_PROGRESS);
 
-		Mediator.getInstance().getProgressNotifier().notifyListeners(
-				ProgressInfo.CLEAR_PROGRESS);
-	}
+        Mediator.getInstance().getProgressNotifier().notifyListeners(ProgressInfo.CLEAR_PROGRESS);
+    }
 
-	// Creates a stop request listener to interrupt the model creation.
-	private Listener<StopRequestMessage> createStopRequestListener() {
-		return new Listener<StopRequestMessage>() {
-			@Override
-			public void update(StopRequestMessage info) {
-				model.interrupt();
-			}
+    // Creates a stop request listener to interrupt the model creation.
+    private Listener<StopRequestMessage> createStopRequestListener() {
+        return new Listener<StopRequestMessage>() {
+            @Override
+            public void update(StopRequestMessage info) {
+                model.interrupt();
+            }
 
-			@Override
-			public boolean canBeRemoved() {
-				return true;
-			}
-		};
-	}
+            @Override
+            public boolean canBeRemoved() {
+                return true;
+            }
+        };
+    }
 }
