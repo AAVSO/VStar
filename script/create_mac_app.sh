4f1866b1b10cbe20ad29072d93b1bfe90bfcb113
David Benn <dbenn@computer.org>
2025-06-12 20:42:46 +0930

    updated JFreeChart version in Mac app creation script

--- script/create_mac_app.sh
+++ script/create_mac_app.sh
@@ -13,7 +13,7 @@ cp -r dist VStar.app/Contents/MacOS
 mkdir VStar.app/Contents/MacOS/extlib
 cp extlib/commons-math-2.2.jar VStar.app/Contents/MacOS/extlib
 cp extlib/javacsv.jar VStar.app/Contents/MacOS/extlib
-cp extlib/jfreechart-1.5.2.jar VStar.app/Contents/MacOS/extlib
+cp extlib/jfreechart-1.5.6.jar VStar.app/Contents/MacOS/extlib
 cp extlib/jmathplot.jar VStar.app/Contents/MacOS/extlib
 cp extlib/antlr-runtime-4.5.3.jar VStar.app/Contents/MacOS/extlib
 cp extlib/nico-camargo-toolbar-icons-4.0.jar VStar.app/Contents/MacOS/extlib
